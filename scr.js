717ee1c8bc13df19e33ec935e6c255ed96c31358
Dmitrukk <47752464+Dmitrukk@users.noreply.github.com>
2019-11-21 15:28:51 +0300

    Add files via upload

---
+++ scr.js
@@ -0,0 +1,18 @@
+const arrayList = [];
+
+function addNewItem(item) {
+    arrayList.push(item);
+  }
+
+function main() {
+    addNewItem("str");
+    addNewItem("dgs");
+    addNewItem("spardg");
+    addNewItem("spa");
+    addNewItem("spardg");
+    addNewItem("adg");
+    addNewItem("str");
+    console.log (arrayList);
+}
+
+main ();
